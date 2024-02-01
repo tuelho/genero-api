849eda2d112f7a180b09eb2b2bb7b7dba0495490
Eduardo Jordano <eduardo.jordano@risoluto.com.br>
2024-02-01 16:21:15 -0300

    Tratamentos de eeros por acento e espaço de nome completo

--- app/Controllers/Http/NomesController.ts
+++ app/Controllers/Http/NomesController.ts
@@ -3,14 +3,14 @@ import Database from '@ioc:Adonis/Lucid/Database'
 export default class NomesController {
 
   async search({  params , response }) {
-    const searchParam = `%${params.pesquisa}%`;
+    const searchParam = `%${decodeURIComponent(params.pesquisa)}%`;
     console.log(searchParam)
     try {
       const { rows } = await Database.rawQuery('select case\n' +
         '      when coalesce(sum(frequency_female), 0)  > coalesce(sum(frequency_male), 0) then \'fem\'\n' +
         '      when coalesce(sum(frequency_male), 0) > coalesce(sum(frequency_female), 0)  then \'masc\'\n' +
         '    end\n' +
-        `from nomes where nomes.alternative_names ilike '${searchParam}';`)
+        `from nomes where nomes.alternative_names ilike split_part(unaccent('${searchParam}'), ' ', 1);`)
       if(rows[0]) {
         return response.json(rows[0].case);
       }
